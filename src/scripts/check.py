e5c9641c43a7e245daad9ef1f53afe80947d3b6c
13993807_admin <13993807_admin@UTS038458.adsroot.uts.edu.au>
2024-09-03 13:43:39 +1000

    load models and prediction

--- src/scripts/check.py
+++ src/scripts/check.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
 
-gpus = tf.config.list_physical_devices('GPU')
+gpus = tf.config.list_physical_devices("GPU")
 print(f"Number of GPUs available: {len(gpus)}")
+
+print(tf.__version__)
